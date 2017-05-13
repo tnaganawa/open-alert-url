dd0eb147b0cf76b16a8b1a47e7c48308fd3c25f4
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-05-13 21:41:27 +0900

    added logic to upload file to jupyter

--- index.py
+++ index.py
@@ -1,22 +1,62 @@
 #!/usr/bin/python
+import json
+import requests
 from flask import Flask, render_template
 app = Flask(__name__)
 
-urlfile='urlfile.txt'
-# $ echo "http://www.google.co.jp" >> urlfile.txt
+
+
+###
+jupyter_url='http://127.0.0.1:8888'
+alertfile='urlfile.txt'
+basepath='/var/tmp/open-alert-url/'
+notebookdir=basepath+'notebooks/'
+###
+# $ echo "test-alert" >> urlfile.txt
+# $ echo "jupyter-upload-test" >> urlfile.txt
+
+alert_to_notebook={
+ "jupyter-upload-test" : "jupyter-upload.ipynb"
+}
+alert_to_url={
+ "test-alert" : "http://www.google.co.jp"
+}
 
 @app.route("/")
 def index():
-    with open(urlfile) as f:
+    # read alertfile
+    alerts=[]
+    with open(alertfile) as f:
      tmp=f.read()
      if (len(tmp)==0):
-      urls=[] 
+      pass
      else:
-      urls=tmp.split('\n')
-      if ('' in urls):
-       urls.remove('')
-    with open(urlfile, 'w') as f:
+      alerts=tmp.split('\n')
+      if ('' in alerts):
+       alerts.remove('')
+
+    # null clear alertfile
+    with open(alertfile, 'w') as f:
      f.write('')
+
+    # create urllist
+    urls=[]
+    for alert in alerts:
+     if (alert in alert_to_url):
+      urls.append(alert_to_url[alert])
+     elif (alert in alert_to_notebook):
+      notebookname=alert_to_notebook[alert]
+      notebookpath=notebookdir+notebookname
+      with open(notebookpath) as f:
+       notebook_content = json.load(f)
+       notebook = {"content": notebook_content}
+       return_value=requests.put(jupyter_url+'/api/contents/{0}'.format(notebookname), data=json.dumps(notebook))
+       #print (return_value)
+       #print (return_value.content)
+       if (199 < return_value.status_code < 299):
+        urls.append('{0}/notebooks/{1}'.format(jupyter_url, notebookname))
+     else:
+      print ("No such alert: {0}", alert)
     return render_template("index.html", urls=urls)
 
 if __name__ == "__main__":
